e02cbbc07563017eba99c51d69acf8971ead7abf
Raúl Gómez Díaz <ragodi666@gmail.com>
2019-11-14 18:42:36 +0100

    Agregamos módulos

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SharedModule } from '../shared/shared.module';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { PagesComponent } from './pages.component';
+
+import { PAGES_ROUTES } from './pages.routes';
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+    PagesComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    PAGES_ROUTES
+  ],
+  exports: [
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+    PagesComponent
+  ]
+})
+export class PagesModule { }
